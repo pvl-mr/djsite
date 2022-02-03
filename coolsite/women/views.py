6074ae6163f86281092dd6bb94dc1719cc0837c0
pvl-mr <mariyapavlova2001@mail.ru>
2022-02-03 20:14:30 +0400

    add login and logout

--- coolsite/women/views.py
+++ coolsite/women/views.py
@@ -1,5 +1,7 @@
+from django.contrib.auth import logout, login
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.views import LoginView
 from django.core.paginator import Paginator
 from django.http import HttpResponse, HttpResponseNotFound, Http404
 from django.shortcuts import render, redirect, get_object_or_404
@@ -92,8 +94,8 @@ def contact(request):
     return HttpResponse("Page for contacting")
 
 
-def login(request):
-    return HttpResponse("Page for login")
+# def login(request):
+#     return HttpResponse("Page for login")
 
 
 def show_post(request, post_slug):
@@ -162,6 +164,27 @@ class RegisterUser(DataMixin, CreateView):
         c_def = self.get_user_context(title='Регистрация')
         return dict(list(context.items()) + list(c_def.items()))
 
+    def form_valid(self, form):
+        user = form.save()
+        login(self.request, user)
+        return redirect('home')
+
+class LoginUser(DataMixin, LoginView):
+    # form_class = AuthenticationForm
+    form_class = LoginUserForm
+    template_name = 'women/login.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        c_def = self.get_user_context(title='Авторизация')
+        return {**context, **c_def}
+
+    def get_success_url(self):
+        return reverse_lazy('home')
+
+def logout_user(request):
+    logout(request)
+    return redirect('login')
 
 def categories(request, catid):
     print(request.GET)
