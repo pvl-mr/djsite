ffbbba95005af3e3884f086f13c451eaf6b7c615
pvl-mr <mariyapavlova2001@mail.ru>
2022-02-05 17:05:49 +0400

    setup an admin panel

--- coolsite/women/admin.py
+++ coolsite/women/admin.py
@@ -1,14 +1,25 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
 
 from .models import *
 
 class WomenAdmin(admin.ModelAdmin):
-    list_display = ('id', 'title', 'time_create', 'photo', 'is_published')
+    list_display = ('id', 'title', 'time_create', 'get_html_photo', 'is_published')
     list_display_links = ('id', 'title')
     search_fields = ('title', 'content')
     list_editable = ('is_published', )
     list_filter = ('is_published', 'time_create')
     prepopulated_fields = {"slug": ("title",)}
+    # fields содержит порядок и список редактируемых полей, которые следует отображать на форме
+    fields = ('title', 'slug', 'cat', 'content', 'photo', 'is_published', 'get_html_photo', 'time_create', 'time_update')
+    readonly_fields = ('time_create', 'time_update', 'get_html_photo')
+    save_on_top = True
+
+    def get_html_photo(self, object):
+        if object.photo:
+            return mark_safe(f"<img src='{object.photo.url}' width=50>")
+
+    get_html_photo.short_description = "Миниатюра"
 
 
 class CategorynAdmin(admin.ModelAdmin):
@@ -19,3 +30,6 @@ class CategorynAdmin(admin.ModelAdmin):
 
 admin.site.register(Women, WomenAdmin)
 admin.site.register(Category, CategorynAdmin)
+
+admin.site.site_title = 'Админ панель сайта о Вайсе'
+admin.site.site_header = 'Админ панель сайта о Вайce 2'
